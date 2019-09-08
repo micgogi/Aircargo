4fecaadee028d43964f89cd230ddd33d2b719756
730058 <Rahul.Gogyani@cognizant.com>
2019-09-08 16:16:13 +0530

    first

---
+++ src/main/java/com/micgogi/aircargo/AircargoApplication.java
@@ -0,0 +1,13 @@
+package com.micgogi.aircargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircargoApplication.class, args);
+	}
+
+}
